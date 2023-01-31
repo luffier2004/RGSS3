13fa686fca7b579681e9558cfe40535dec3edaf1
luffier2004 <30382012+luffier2004@users.noreply.github.com>
2023-02-01 01:56:45 +0800

    Add files via upload

---
+++ 修改游戏字体.rb
@@ -0,0 +1,11 @@
+Font.default_name = [""]
+Font.default_size = 22
+Font.default_bold = false
+Font.default_italic = false
+Font.default_color = Color.new(255, 255, 255, 255)
+Font.default_outline = true
+class Window_Base < Window
+  def normal_color
+    return Font.default_color
+  end
+end
